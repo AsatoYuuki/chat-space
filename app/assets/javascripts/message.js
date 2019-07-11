cb9aac50a63834fe116e239ad2fc67e907099788
AsatoYuuki <yuuki.ast@icloud.com>
2019-07-11 12:59:45 +0900

    グループ.インクルメンタル

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,25 +1,24 @@
 $(function() {
-  $(document).on('turbolinks:load', function(){
-    function buildHTML(message) {
-      var body = message.body ? `${ message.body }` : "";
-      var img = message.image ? `<img src= ${ message.image }>` : "";
-      var html = `<div class="message__box" data-id="${message.id}">
-                    <div class="message__box__for">
-                        <p class="message__box__for__name">
-                          ${message.user_name}
-                        </p>
-                        <p class="message__box__for__info">
-                          ${message.date}
-                        </p>
-                      </div>
-                    <div class="message__box__text">
-                      <p class="message__box__text__view">
-                        ${body}
+  function buildHTML(message) {
+    var body = message.body ? `${ message.body }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message__box" data-id="${message.id}">
+                  <div class="message__box__for">
+                      <p class="message__box__for__name">
+                        ${message.user_name}
                       </p>
-                        ${img}
-                    </div>`
-    return html;
-    }
+                      <p class="message__box__for__info">
+                        ${message.date}
+                      </p>
+                    </div>
+                  <div class="message__box__text">
+                    <p class="message__box__text__view">
+                      ${body}
+                    </p>
+                      ${img}
+                  </div>`
+  return html;
+  }
     // $(document).on('turbolinks:load', function(){
       // Rails5を使っている方はturbolinksが悪さをしていることがあるので$(document).ready(function(){})ではなくturbolinksを初回読み込み、リロード、ページ切り替えで動くように上のように記述しましょう。
     // そうすることによってページを一回読み込まないと上手くいかないという事がなくなるはずです。
@@ -49,7 +48,7 @@ $(function() {
       .always(function(data){
         $('.form__new_message__sent').prop('disabled', false);//ここで解除している
         
-      })
-    });
     })
   });
+});
+
